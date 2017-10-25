aa2042ac220260584b20bc9b5ac6f4c27fa8a283
Matt Scott <mscott232@gmail.com>
2017-10-25 12:01:48 -0500

    Added ability to read in text file, get maze size, starting point and convert to 2d char array

--- MazeAssignment/Program.cs
+++ MazeAssignment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MazeAssignment
 {
@@ -10,6 +11,22 @@ namespace MazeAssignment
     {
         static void Main(string[] args)
         {
+            String[] lines = File.ReadAllLines(@"C:\Users\Matt\Documents\School\Term 5\Programming 4\Assignments\Assignment 2\2 Final Milestone\maze.maze");
+
+            int[] dimensions = lines[0].Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            int[] startingPoint = lines[1].Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+            char[,] maze = new char[dimensions[0], dimensions[1]];
+
+            for(int i = 2; i < lines.Length; i++)
+            {
+                char[] items = lines[i].ToCharArray();
+
+                for(int j = 0; j < items.Length; j++)
+                {
+                    maze[i - 2, j] = items[j];
+                }
+            }
         }
     }
 }
